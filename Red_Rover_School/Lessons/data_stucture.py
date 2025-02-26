052698495b195fe91fcc817c52087464ca9ef935
Stanislav Kaplya <staskaplya@gmail.com>
2025-02-26 13:42:06 +0100

    init

---
+++ Red_Rover_School/Lessons/data_stucture.py
@@ -0,0 +1,17 @@
+# a = [1,2,3]
+# b = [1,2,3]
+#
+# print(a == b)  #True
+# print(a is b)  #False проверяет на одинаковость обьектов
+
+
+#++++++++++++++++++SET+++++++++++++
+
+#set = {} # нельзя так создать сет
+set = set() # create set
+set.add(3)
+set.add(4)
+set.add(4)
+set.add(5)
+print(set)   #{3, 4, 5}
+
