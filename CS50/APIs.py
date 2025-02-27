6b985b67674d94e538b91d970ab63f2c520cd06e
Stanislav Kaplya <staskaplya@gmail.com>
2025-02-27 22:22:27 +0100

    init

---
+++ CS50/APIs.py
@@ -0,0 +1,15 @@
+import json
+import requests
+import sys
+
+#достаем по api информацию о ссписке песен исполнителя weezor(вводим все в терминале)
+
+if len(sys.argv)!=2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
+
+o = response.json()
+for result in o['results']:
+    print(result['trackName'])
