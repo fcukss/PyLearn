0de16ec93b04f67f7b841f5c29639be563e6e04c
Stanislav Kaplya <staskaplya@gmail.com>
2025-03-18 13:39:43 +0100

    init

--- Red_Rover_School/Lessons/lesson7/lesson7_1.py
+++ Red_Rover_School/Lessons/lesson7/lesson7_1.py
@@ -22,42 +22,42 @@
 from itertools import count
 
 print("================================================")
-
-class User:
-    def __init__(self,n, surname, age=30):
-        self.name = n
-        self.surname = surname
-        self.age = age
-        self.money=0
-
-    def greet(self):
-        print(f"Hello, {self.name}!")
-
-    def birthday(self):
-        self.age +=1
-
-    def add_money(self, n):
-        self.money+=n
-
-user1 = User("Stas", "Kaplia")
-user2 = User("Kate", "Khoklova", 38)
-user3 = User("Tom","Soyer", 16)
-
-print(user1.name, user1.surname, user1.age)   #Stas
-print(user2.name, user2.age)
-
-user1.greet()
-
-print(user1.age)    #30
-user1.birthday()
-print(user1.age)    #31
-
-
-print(user1.money)   #0
-user1.add_money(100)
-print(user1.money)   #100
-
-
+#
+# class User:
+#     def __init__(self,n, surname, age=30):
+#         self.name = n
+#         self.surname = surname
+#         self.age = age
+#         self.money=0
+#
+#     def greet(self):
+#         print(f"Hello, {self.name}!")
+#
+#     def birthday(self):
+#         self.age +=1
+#
+#     def add_money(self, n):
+#         self.money+=n
+#
+# user1 = User("Stas", "Kaplia")
+# user2 = User("Kate", "Khoklova", 38)
+# user3 = User("Tom","Soyer", 16)
+#
+# print(user1.name, user1.surname, user1.age)   #Stas
+# print(user2.name, user2.age)
+#
+# user1.greet()
+#
+# print(user1.age)    #30
+# user1.birthday()
+# print(user1.age)    #31
+#
+#
+# print(user1.money)   #0
+# user1.add_money(100)
+# print(user1.money)   #100
+#
+#
 
 
 
