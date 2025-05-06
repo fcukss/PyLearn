b262b7664af8e732544abb9265a9be16b56884e6
fcukss <staskaplya@gmail.com>
2025-05-06 23:43:54 +0200

    update

--- CS50/OOP/HW/seasons_of_love.py
+++ CS50/OOP/HW/seasons_of_love.py
@@ -1,11 +1,30 @@
+import re
+import sys
 from datetime import date
-
+import inflect
 
 def main():
-    ...
+    number = get_minutes()
+    p = inflect.engine()
+    print(f'{p.number_to_words(number).capitalize().replace(' and ',' ')} minutes')
+
+def get_minutes():
+    input_date = is_valid(input("Date of Birth: "))
+    delta = date.today() - input_date
+    days = abs(delta.days)
+    minutes = days*24*60
+    return minutes
 
 
-...
+def is_valid(text):
+    pattern = r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$"
+    if re.match(pattern, text):
+        try:
+            return date.fromisoformat(text)
+        except ValueError:
+            sys.exit("Invalid date")
+    else:
+        sys.exit("Invalid date")
 
 
 if __name__ == "__main__":
---
+++ CS50/OOP/HW/test_seasons.py
@@ -0,0 +1,6 @@
+import sys
+
+import seasons_of_love
+
+def test_is_valid():
+    assert seasons_of_love.is_valid('2022-20-80') == False
